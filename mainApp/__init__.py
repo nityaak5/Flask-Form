82443618df7d632619a053f4474ad1ae290bebe0
nityaak5 <60267051+nityaak5@users.noreply.github.com>
2021-07-14 21:24:50 +0530

    Update __init__.py

--- mainApp/__init__.py
+++ mainApp/__init__.py
@@ -14,8 +14,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'nityaakalra5@gmail.com'
-app.config['MAIL_PASSWORD'] = 'stargirlnityaa'
+app.config['MAIL_USERNAME'] = email
+app.config['MAIL_PASSWORD'] = password
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
